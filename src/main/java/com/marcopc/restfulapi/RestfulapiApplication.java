26ccb4b8f404a31f2ede9c8dbda334402c9bfbf1
mqrcoo <marcoa13310@gmail.com>
2025-11-02 13:59:57 -0400

    creacion de yml + uso de postgres como db

--- src/main/java/com/marcopc/restfulapi/RestfulapiApplication.java
+++ src/main/java/com/marcopc/restfulapi/RestfulapiApplication.java
@@ -3,7 +3,7 @@ package com.marcopc.restfulapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//El flujo de acceso a los datos es: Repository (acceso a db) -> Service (reglas de negocio) -> Controller (endpoint)
+//El flujo de acceso a los datos es: Repository (acceso a datos en la db) -> Service (reglas de negocio) -> Controller (endpoint)
 
 //Inversion de dependencias es basicamente utilizar las funcionalidades del framework, como se usa JPA en vez de JPQL en el metodo findByName...
 
